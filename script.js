79f8b7ec62cb3cc003081c94c7ebac311c03f75d
Melina <melina.dmec@gmail.com>
2026-03-25 10:14:59 +0100

    ejercicio 1

---
+++ script.js
@@ -0,0 +1,12 @@
+// Definir 2 variables de tipo numérico
+let numero1 = 10;
+let numero2 = 20;
+
+// Condicionales para comparar
+if (numero1 > numero2) {
+    console.log("El número 1 es mayor");
+} else if (numero2 > numero1) {
+    console.log("El número 2 es mayor");
+} else {
+    console.log("Los números son iguales");
+}
